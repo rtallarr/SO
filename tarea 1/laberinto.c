e1e640fdb5958c483242b933c433e76d81f4ff53
rtallarr <rtallarr@gmail.com>
2021-09-22 19:55:51 -0300

    update

--- tarea 1/laberinto.c
+++ tarea 1/laberinto.c
@@ -44,6 +44,8 @@ int main() {
         chdir("..");
     }
 
+    //FILE 1
+
     int num1 = rando(3, 1); //numero 3-1
     int num2 = rando(3, 1);
     int num3 = rando(3, 1);
@@ -52,7 +54,7 @@ int main() {
     char NUM2[2];
     char NUM3[2];
 
-    printf("num1: %d\nnum2: %d\nnum3: %d\n", num1, num2, num3);
+    //printf("num1: %d\nnum2: %d\nnum3: %d\n", num1, num2, num3);
 
     snprintf(NUM1, sizeof(NUM1), "%d", num1); //int->str
     snprintf(NUM2, sizeof(NUM2), "%d", num2);
@@ -62,10 +64,38 @@ int main() {
     chdir(NUM2);
     chdir(NUM3);
 
-    FILE *fp;
-    fp = fopen("frase.txt", "w");
-    fprintf(fp, "1, los hurones del \n");
-    fclose(fp);
+    FILE *fp1;
+    fp1 = fopen("frase.txt", "w");
+    fprintf(fp1, "1, los hurones del \n");
+    fclose(fp1);
+
+    //FILE 2
+
+    num1 = rando(3, 1); //numero 3-1
+    num2 = rando(3, 1);
+    num3 = rando(3, 1);
+
+    snprintf(NUM1, sizeof(NUM1), "%d", num1); //int->str
+    snprintf(NUM2, sizeof(NUM2), "%d", num2);
+    snprintf(NUM3, sizeof(NUM3), "%d", num3);
+
+    if (access("frase.txt", F_OK ) == 0 ) {
+        num1 = rando(3, 1); 
+        num2 = rando(3, 1);
+        num3 = rando(3, 1);
+        snprintf(NUM1, sizeof(NUM1), "%d", num1);
+        snprintf(NUM2, sizeof(NUM2), "%d", num2);
+        snprintf(NUM3, sizeof(NUM3), "%d", num3);
+    }
+
+    chdir(NUM1);
+    chdir(NUM2);
+    chdir(NUM3);
+    
+    FILE *fp2;
+    fp2 = fopen("frase.txt", "w");
+    fprintf(fp2, "2, profesor de sistemas operativos son \n");
+    fclose(fp2);
 
     return 0;
 }
