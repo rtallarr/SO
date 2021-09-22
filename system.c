2155432bf61e3dab064a8ed79e6b2920228203bb
rtallarr <rtallarr@gmail.com>
2021-09-22 12:33:41 -0300

    update

--- system.c
+++ system.c
@@ -1,16 +1,23 @@
-#include <stdio.h> //system()
-#include <stdlib.h> 
+#include <stdio.h>
+#include <stdlib.h> //system()
 #include <string.h> //strcat()
+#include <unistd.h> //chdir()
 
 int main() {
     //primera capa
-    //system("mkdir 1");
+    system("mkdir 1");
     //system("mkdir 2");
     //system("mkdir 3");
 
     //segunda capa
-    system("cd 1");
+    char string[25];
+    chdir("1");
+    for(int i=1; i<=3; i++){
+        snprintf(string, sizeof(string), "%d", i);
+        //strcat("mkdir", string);
+        printf("%d\n", i);
+    }
 
-    system("ls");
+    //system("ls");
     return 0;
 }
