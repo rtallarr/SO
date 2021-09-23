f7dd3ec55e73da848de4d94ad55d764c28d719c8
rtallarr <rtallarr@gmail.com>
2021-09-22 22:38:06 -0300

    laberinto finished

--- tarea 1/laberinto.c
+++ tarea 1/laberinto.c
@@ -2,6 +2,7 @@
 #include <stdlib.h> //system()
 #include <unistd.h> //chdir()
 #include <time.h> //time()
+#include <string.h>
 
 void create_dirs(){
     system("mkdir 1");
@@ -13,6 +14,17 @@ void create_dirs(){
 int rando(int up, int low){
     int num = (rand() % (up - low + 1)) + low;
     return num;
+}
+
+char* concatenate(char* var, int one, int two, int three){
+    char buffer2[10];
+    char buffer3[10];
+    snprintf(var, 10, "%d", one); //int->str
+    snprintf(buffer2, sizeof(buffer2), "%d", two); //int->str
+    snprintf(buffer3, sizeof(buffer3), "%d", three);
+    strcat(var, buffer2);
+    strcat(var, buffer3);
+    return var;
 }
 
 int main() {
@@ -55,19 +67,32 @@ int main() {
     char NUM3[2];
 
     //printf("num1: %d\nnum2: %d\nnum3: %d\n", num1, num2, num3);
+    //char s[150];
+    //printf("working dir: %s\n", getcwd(s, 100));
 
     snprintf(NUM1, sizeof(NUM1), "%d", num1); //int->str
     snprintf(NUM2, sizeof(NUM2), "%d", num2);
     snprintf(NUM3, sizeof(NUM3), "%d", num3);
+
+    char conca1[10];
+    concatenate(conca1, num1, num2, num3); //concatenate combination of folders 
+    printf("conca1: %s\n", conca1);
     
     chdir(NUM1);
     chdir(NUM2);
     chdir(NUM3);
 
+    //char s[150];
+    //printf("working dir: %s\n", getcwd(s, 100));
+
     FILE *fp1;
     fp1 = fopen("frase.txt", "w");
-    fprintf(fp1, "1, los hurones del \n");
+    fprintf(fp1, "1, los hurones del ");
     fclose(fp1);
+
+    chdir("..");
+    chdir("..");
+    chdir("..");
 
     //FILE 2
 
@@ -75,27 +100,71 @@ int main() {
     num2 = rando(3, 1);
     num3 = rando(3, 1);
 
+    char conca2[10];
+    concatenate(conca2, num1, num2, num3); //concatenate combination of folders 
+    printf("conca2: %s\n", conca2);
+
+    while (conca1 == conca2) {
+        num1 = rando(3, 1); //volver a generar los numeros
+        num2 = rando(3, 1);
+        num3 = rando(3, 1);
+    }
+
     snprintf(NUM1, sizeof(NUM1), "%d", num1); //int->str
     snprintf(NUM2, sizeof(NUM2), "%d", num2);
     snprintf(NUM3, sizeof(NUM3), "%d", num3);
-
-    if (access("frase.txt", F_OK ) == 0 ) {
-        num1 = rando(3, 1); 
-        num2 = rando(3, 1);
-        num3 = rando(3, 1);
-        snprintf(NUM1, sizeof(NUM1), "%d", num1);
-        snprintf(NUM2, sizeof(NUM2), "%d", num2);
-        snprintf(NUM3, sizeof(NUM3), "%d", num3);
-    }
 
     chdir(NUM1);
     chdir(NUM2);
     chdir(NUM3);
     
+    //char r[150];
+    //printf("working dir: %s\n", getcwd(r, 100));
+
     FILE *fp2;
     fp2 = fopen("frase.txt", "w");
-    fprintf(fp2, "2, profesor de sistemas operativos son \n");
+    fprintf(fp2, "2, profesor de sistemas operativos son "); //3, un mob del minecraft altamente peligroso y temido por millones
     fclose(fp2);
+
+    chdir("..");
+    chdir("..");
+    chdir("..");
+
+    //FILE 3
+
+    num1 = rando(3, 1); //numero 3-1
+    num2 = rando(3, 1);
+    num3 = rando(3, 1);
+
+    char conca3[10];
+    concatenate(conca3, num1, num2, num3); //concatenate combination of folders 
+    printf("conca3: %s\n", conca3);
+
+    while (conca1 == conca3 || conca2 == conca3) {
+        num1 = rando(3, 1); //volver a generar los numeros
+        num2 = rando(3, 1);
+        num3 = rando(3, 1);
+    }
+
+    snprintf(NUM1, sizeof(NUM1), "%d", num1); //int->str
+    snprintf(NUM2, sizeof(NUM2), "%d", num2);
+    snprintf(NUM3, sizeof(NUM3), "%d", num3);
+
+    chdir(NUM1);
+    chdir(NUM2);
+    chdir(NUM3);
+    
+    //char t[150];
+    //printf("working dir: %s\n", getcwd(t, 100));
+
+    FILE *fp3;
+    fp3 = fopen("frase.txt", "w");
+    fprintf(fp3, "3, un mob del minecraft altamente peligroso y temido por millones");
+    fclose(fp3);
+
+    chdir("..");
+    chdir("..");
+    chdir("..");
 
     return 0;
 }
