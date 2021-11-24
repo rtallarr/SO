9f25d119df004023ed037647fef3ddb983c9a3f7
rtallarr <rtallarr@gmail.com>
2021-11-23 22:02:12 -0300

    threads

--- TAREA3/tarea.java
+++ TAREA3/tarea.java
@@ -1,5 +1,28 @@
-public class tarea {
-  public static void main(String[] args){
-    System.out.println("This is my first program in java");
-  }//End of main
-}//End of FirstJavaProgram Class
+// Java code for thread creation by extending
+// the Thread class
+class MultithreadingDemo extends Thread {
+    public void run()
+    {
+        try {
+            // Displaying the thread that is running
+            System.out.println(
+                "Thread " + Thread.currentThread().getId() + " is running");
+        }
+        catch (Exception e) {
+            // Throwing an exception
+            System.out.println("Exception is caught");
+        }
+    }
+}
+ 
+// Main Class
+public class main {
+    public static void main(String[] args)
+    {
+        int n = 3; // Number of threads
+        for (int i = 0; i < n; i++) {
+            MultithreadingDemo object = new MultithreadingDemo();
+            object.start();
+        }
+    }
+}
