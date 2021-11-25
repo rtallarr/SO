a2aa58c45eca959f3ec2c03c0c5b488e3cc19ad7
rtallarr <rtallarr@gmail.com>
2021-11-25 20:27:16 -0300

    update

--- TAREA3/tarea.java
+++ TAREA3/tarea.java
@@ -1,28 +1,94 @@
+import java.util.Scanner;
+import java.time.LocalTime;
+import java.io.File;
+import java.io.FileNotFoundException;
+
 // Java code for thread creation by extending
 // the Thread class
-class MultithreadingDemo extends Thread {
-    public void run()
-    {
-        try {
-            // Displaying the thread that is running
-            System.out.println(
-                "Thread " + Thread.currentThread().getId() + " is running");
+class SimpleThread extends Thread {
+    public SimpleThread(String name){
+        super(name);
+    }
+
+    public void run() {
+        if (getName() == "t1"){ //hebra 1
+            //System.out.println("aki t1. Hebra n° " + Thread.currentThread().getId());
+            try {
+                File myObj = new File("palabras.txt");
+                Scanner myReader = new Scanner(myObj);
+                while (myReader.hasNextLine()) {
+                    String[] data = myReader.nextLine().split(" ");
+                    for (String a : data){
+                        if (a.equals("eiffel")){
+                            System.out.println(a);
+                        }
+                    }
+                }
+                myReader.close();
+            } catch (FileNotFoundException e) {
+                System.out.println("An error occurred.");
+                e.printStackTrace();
+            }
+            LocalTime fin1 = java.time.LocalTime.now();
+            System.out.println("Hebra 1 terminando a las "+ fin1);
         }
-        catch (Exception e) {
-            // Throwing an exception
-            System.out.println("Exception is caught");
+
+        if (getName() == "t2"){ //hebra 2
+            try {
+                File myObj = new File("numeros.txt");
+                Scanner myReader = new Scanner(myObj);
+                while (myReader.hasNextLine()) {
+                    String[] data = myReader.nextLine().split(" ");
+                    for (String a : data){
+                        if (a.equals("eiffel")){
+                            System.out.println(a);
+                        }
+                    }
+                }
+                myReader.close();
+            } catch (FileNotFoundException e) {
+                System.out.println("An error occurred.");
+                e.printStackTrace();
+            }
+            LocalTime fin2 = java.time.LocalTime.now();
+            System.out.println("Hebra 2 terminando a las "+ fin2);
+        }
+        if (getName() == "t3"){ //hebra 3
+            try {
+                File myObj = new File("morse.txt");
+                Scanner myReader = new Scanner(myObj);
+                while (myReader.hasNextLine()) {
+                    String[] data = myReader.nextLine().split(" ");
+                    for (String a : data){
+                        if (a.equals("eiffel")){
+                            System.out.println(a);
+                        }
+                    }
+                }
+                myReader.close();
+            } catch (FileNotFoundException e) {
+                System.out.println("An error occurred.");
+                e.printStackTrace();
+            }
+            LocalTime fin3 = java.time.LocalTime.now();
+            System.out.println("Hebra 3 terminando a las "+ fin3);
         }
     }
 }
  
 // Main Class
-public class main {
-    public static void main(String[] args)
-    {
-        int n = 3; // Number of threads
-        for (int i = 0; i < n; i++) {
-            MultithreadingDemo object = new MultithreadingDemo();
-            object.start();
-        }
+public class tarea {
+    public static void main(String[] args) {
+        LocalTime inicio1 = java.time.LocalTime.now();
+        System.out.println("\nHebra 1 iniciando a las " + inicio1 + "\n");
+        new SimpleThread("t1").start();
+
+        //LocalTime inicio2 = java.time.LocalTime.now();
+        //System.out.println("Hebra 2 iniciando a las " + inicio2 + "\n");
+        //new SimpleThread("t2").start();
+//
+        //LocalTime inicio3 = java.time.LocalTime.now();
+        //System.out.println("Hebra 3 iniciando a las " + inicio3 + "\n");
+        //new SimpleThread("t3").start();
     }
 }
